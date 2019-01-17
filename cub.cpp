6674a92e06f586845f6fdba6061329f64aaef2d5
Centrix15 <46788532+Centrix15@users.noreply.github.com>
2019-01-17 22:27:38 +0300

    Update cub.cpp

--- cub.cpp
+++ cub.cpp
@@ -1,1 +1,12 @@
+#include "Cub.h"
 
+//конструктор с 1 аргументом
+Cub::Cub(){
+	int* width = new int;
+	int* height = new int;
+	
+	*width = 3;
+	*height = 3;
+	
+	int* kletki_cubs = new int [NumberStoron][*width][*height];
+}
