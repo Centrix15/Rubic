0fe6ae496dd189d1bb097b4c91819e7b1e828614
Centrix15 <46788532+Centrix15@users.noreply.github.com>
2019-01-20 20:15:51 +0300

    Update elmsCube.cpp

--- elmsCube.cpp
+++ elmsCube.cpp
@@ -3,12 +3,14 @@
 using namespace std;
 
 //вывод elementCub, используя потоки
+
 friend ostream& operator<< (ostream& s, elementCub& e){
-	s << '\t' << e.arrElm << endl;
-	s << '\t' << e.arrElm << endl;
-	s << e.arrElm << "  ";
-	s << e.arrElm << "  ";
 	s << e.arrElm << endl;
-	s << '\t' << e.arrElm << endl;
+	s << e.arrElm << ' ';
+	s << e.arrElm << ' ';
+	s << e.arrElm << ' ';
+	s << e.arrElm << endl;
+	s << e.arrElm << endl;
+	s << "location: " << *(location) << endl;
 	return e;	
 }
